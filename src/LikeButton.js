3f7c25d6d74eacb10ee49ba782931b0718c40e1b
https://github.com/harpalvaghela <harpalvaghela18@gmail.com>
2024-03-12 09:05:32 -0400

    First Commit

---
+++ src/LikeButton.js
@@ -0,0 +1,22 @@
+// LikeButton.js
+import React, { useState } from "react";
+import "./App.css";
+
+function LikeButton() {
+  const [likes, setLikes] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <button
+      className={`like-button ${liked ? 'liked' : ''}`}
+      onClick={() => {
+        setLikes(likes + 1);
+        setLiked(true);
+      }}
+    >
+      {likes} Likes
+    </button>
+  );
+}
+
+export default LikeButton;
